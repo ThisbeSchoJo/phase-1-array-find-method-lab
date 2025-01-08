c3f0554acee845482bf31f59857a06d9c129038c
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-01-08 16:20:07 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+const record = [
+    {year:"2015", result:"W"},
+    {year:"2014", result:"N/A"},
+    {year:"2013", result:"L"}
+]
+
+function superbowlWin(record) {
+    const broncoWins = record.find((element) => {
+        return element.result === "W";
+    });
+    return broncoWins ? broncoWins.year : undefined;
+}
+
+// function superbowlWin(wins) {
+//     const broncoWins = record.find((wins) => {
+//         console.log(broncoWins)
+//         wins.result === "W"
+//     })
+// }
